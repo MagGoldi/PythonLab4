4ecf139caa5e2b89b45a290d2fcb87dc4193591a
MagGoldi <StivenH0king@yandex.ru>
2022-11-30 23:45:41 +0400

    work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
 def task6(df: pd.DataFrame, value: float) -> pd.DataFrame:
     return df.loc[df['temp_day'] >= value]
@@ -10,12 +11,24 @@ def task7(df: pd.DataFrame, value1: float, value2: float) -> pd.DataFrame:
     df =  df.loc[df['temp_day'] <= value2]
     return df
 
+def custom(df: pd.DataFrame, month: int, year: int) -> pd.DataFrame:
+    df['data'] =  df['data'].apply(str) 
+    print(df.dtypes)
+    #df_custom = df.loc[(df['temp_day'] >= 20)]
+    data1 = str(str(year)+"-"+str(month) +"-"+"01")
+    data2 = str(str(year)+"-"+str(month) +"-"+"31")
+    df_custom = df.loc[(df['data'] >= data1) & (df['data'] <= data2)]
+    df_custom = df.loc[(df['data'] <= data2)]
+    print(df_custom)             
+    print(df_custom.info())
+    print(df_custom)  
+    print(df_custom)  
 
 def main() -> None:
 
     df = pd.read_csv("C:\PYTHON\PythonLab4\dataset.csv") #1
 
-    df.columns =['data', 'temp_day', 'wind', 'pressure_day', 'temp_even', 'pressure_even'] #2
+    df.columns = ['data', 'temp_day', 'wind', 'pressure_day', 'temp_even', 'pressure_even'] #2
     
 
     nan_value = float("NaN")                        #3  ( null / None / Nan ) ////
@@ -57,6 +70,12 @@ def main() -> None:
     plt.ylabel("Temp")
     plt.plot(df['temp_f_day'])
     plt.show()
+
+
+    df['data'] =  df['data'].astype(str)  
+    print(df.dtypes)
+    month, year = 8, 2021
+    custom(df, month, year)
 
 
 
--- test.py
+++ test.py
@@ -1,23 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
 
-#create some data
-var1 = [1, 2, 3, 4, 5, 6]
-var2 = [7, 13, 16, 18, 25, 19]
-var3 = [29, 25, 20, 25, 20, 18]
-var4 = [4, 4, 6, 4, 7, 11]
 
-#define grid of plots
-fig, axs = plt.subplots(nrows= 2 , ncols= 2 )
-
-#add title
-fig. suptitle('Grid of Plots')
-
-#add data to plots
-axs[0, 0].plot(var1, var2)
-axs[0, 1].plot(var1, var3)
-axs[1, 0].plot(var1, var4)
-axs[1, 1].plot(var3, var1)
-plt.show()
+print("2021-02-19" >= "2021-02-20")
