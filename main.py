392114d4bfe0a7b6c2b2b6a9bacd7d5a07527397
MagGoldi <StivenH0king@yandex.ru>
2022-12-01 15:16:00 +0400

    work in 9 task

--- main.py
+++ main.py
@@ -7,42 +7,53 @@ def task6(df: pd.DataFrame, value: float) -> pd.DataFrame:
     return df.loc[df['temp_day'] >= value]
 
 def task7(df: pd.DataFrame, value1: float, value2: float) -> pd.DataFrame:
-    df =  df.loc[df['temp_day'] >= value1]
-    df =  df.loc[df['temp_day'] <= value2]
+    df =  df.loc[(df['temp_day'] >= value1) & (df['temp_day'] <= value2)]
     return df
 
 def custom(df: pd.DataFrame, month: int, year: int) -> pd.DataFrame:
-    df['data'] =  df['data'].apply(str) 
-    print(df.dtypes)
-    #df_custom = df.loc[(df['temp_day'] >= 20)]
-    data1 = str(str(year)+"-"+str(month) +"-"+"01")
-    data2 = str(str(year)+"-"+str(month) +"-"+"31")
+    if(month<10): month = "0"+str(month) 
+
+    data1 = str(str(year)+"-"+ month +"-"+"01")
+    data2 = str(str(year)+"-"+ month +"-"+"31")
+
     df_custom = df.loc[(df['data'] >= data1) & (df['data'] <= data2)]
-    df_custom = df.loc[(df['data'] <= data2)]
-    print(df_custom)             
-    print(df_custom.info())
+    test = df_custom.shape[0]
+    df_custom.index = [i for i in range(0, int(test))]
+   
+      
     print(df_custom)  
-    print(df_custom)  
+    plt.violinplot(df_custom['temp_day'], showmedians=True)
+    plt.plot(df_custom['temp_day'])
+    plt.title("Temperature graph in Celsius")  #Fahrenheit temperature graph
+    plt.xlabel("Day")
+    plt.ylabel("Temp")
+    plt.plot(df_custom['temp_day'])
+    plt.show()
+
+
+
 
 def main() -> None:
 
     df = pd.read_csv("C:\PYTHON\PythonLab4\dataset.csv") #1
+    print(df)
 
     df.columns = ['data', 'temp_day', 'wind', 'pressure_day', 'temp_even', 'pressure_even'] #2
+    print(df)
     
-
     nan_value = float("NaN")                        #3  ( null / None / Nan ) ////
     df.replace(" ", nan_value, inplace=True)
     df = df.dropna()
     print(df)
 
+    test = df.shape[0]                              #3.5
+    df.index = [i for i in range(0, int(test))]
+    print(df)
 
     df[['temp_day', "temp_even"]] =  df[['temp_day', "temp_even"]].astype(int)        #4
-    print(df)
     print(df.dtypes)
     df = df.assign(temp_f_day = (df.temp_day *9/5) +32, temp_f_even = (df.temp_even*9/5) +32)
     print(df)
-
 
     stats = df[['temp_day', "temp_even", "temp_f_day", "temp_f_even"]].describe()     #5
     print(stats)
